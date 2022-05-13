ad9cd5357415bcbae5b7095218014e8f32767e72
Papalpatine <davidloisel2001@gmail.com>
2022-05-13 16:02:51 +0200

    maj home

--- views/home.php
+++ views/home.php
@@ -6,36 +6,7 @@
     } 
     ?> </h1>
 <section>
-    <!-- petit comment de test -->
-    <article>
-        <img src="./content/infinite.png" />
-    </article>
-    <aside>
-        <div class="mesures">
-            <div class="element">
-                <img src="./content/noise.png" />
-                <p>Sonore</p>
-                </br></br></br></br>
-                <img src="./content/cardiac.png" />
-                <p>Rythme cardiaque</p>
-            </div>
-
-            <div class="element">
-                <img src="./content/co2.png" />
-                <p>CO2</p>
-            </div>
-
-            <div class="element">
-                <img src="./content/temperature.png" />
-                <p>Température</p>
-                </br></br></br></br>
-                <img src="./content/humidity.png" />
-                <p>Humidité</p>
-            </div>
-        </div>
-    </aside>
     <div class="texts">
-        <div class="Texts2">
     <h1>Présentation</h1>
     <p class="presentation">
     Wave IT propose une interface permettant de comprendre votre environnement.
@@ -43,11 +14,34 @@
     Notre site comprend une interface ludique vous mettant en compétition avec le restant des participants pour avoir le meilleur environnement de travail.
     </p>
         </div>
-        <div class="Texts2">
+    <aside>
+        <div class="mesures">
+            <div class="element">
+                <img src="./content/noise.png" />
+                <p>Sonore</p>
+            </div>
+            <div class="element">
+                <img src="./content/cardiac.png" />
+                <p>Rythme cardiaque</p>
+            </div>
+            <div class="element">
+                <img src="./content/co2.png" />
+                <p>CO2</p>
+            </div>
+            <div class="element">
+                <img src="./content/temperature.png" />
+                <p>Température</p>
+            </div>
+            <div class="element">
+                <img src="./content/humidity.png" />
+                <p>Humidité</p>
+            </div>
+        </div>
+    </aside>
+    <div class="texts">
     <h1>Le Challenge</h1>
     <p class="presentation">
     Leaderboard et compétition, nous allons vous faire aimer le bien-être au travail.   
     </p>
-        </div>
     </div>
 </section>
