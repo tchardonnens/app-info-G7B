e1a6661077ed1e72d32694c19b68254e8306089a
Papalpatine <davidloisel2001@gmail.com>
2022-05-16 13:42:09 +0200

    maj challenge

--- views/challenge.php
+++ views/challenge.php
@@ -11,13 +11,24 @@
     <p>equipe 2</p>
     <p>Score</p>
     <p>flèche</p>
+    <div class="deux">
+    <div class="deux2">
+        <p>2</p>
+    </div>
+</div>
 </equipe2>
+
 
 <equipe1>
     <p>1</p>
     <p>equipe 1</p>
     <p>Score</p>
     <p>flèche</p>
+    <div class="un">
+        <div class="un2">
+        <p>1</p>
+        </div>  
+    </div>
 </equipe1>
 
 <equipe3>
@@ -25,7 +36,13 @@
     <p>equipe 3</p>
     <p>Score</p>
     <p>flèche</p>
+    <div class="trois">
+    <div class="trois2">
+        <p>3</p>
+    </div>
+</div>
 </equipe3>
+
 </div>
 
 
