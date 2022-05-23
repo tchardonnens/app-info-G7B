c4e414672c3b4ebc794b6920544a86593bd5a499
Papalpatine <davidloisel2001@gmail.com>
2022-05-23 11:03:48 +0200

    maj home, challenge et header/footer je crois

--- views/challenge.php
+++ views/challenge.php
@@ -7,38 +7,34 @@
     </head>
 <div class="podium">
 <equipe2>
-    <p>2</p>
     <p>equipe 2</p>
-    <p>Score</p>
-    <p>flèche</p>
     <div class="deux">
     <div class="deux2">
-        <p>2</p>
+        <p class="rang">2</p>
+        <p>Score</p>
+        <p>flèche</p>
     </div>
 </div>
 </equipe2>
 
-
 <equipe1>
-    <p>1</p>
     <p>equipe 1</p>
-    <p>Score</p>
-    <p>flèche</p>
     <div class="un">
         <div class="un2">
-        <p>1</p>
+        <p class="rang">1</p>
+        <p>Score</p>
+        <p>flèche</p>
         </div>  
     </div>
 </equipe1>
 
 <equipe3>
-    <p>3</p>
     <p>equipe 3</p>
-    <p>Score</p>
-    <p>flèche</p>
     <div class="trois">
     <div class="trois2">
-        <p>3</p>
+        <p class="rang">3</p>
+        <p>Score</p>
+        <p>flèche</p>
     </div>
 </div>
 </equipe3>
